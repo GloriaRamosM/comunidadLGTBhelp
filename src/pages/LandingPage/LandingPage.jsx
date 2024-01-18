3971d0ecb66eeb7687cff480fad677bd6e599b94
GloriaRamosM <gloriaramosmoran@gmail.com>
2024-01-18 17:21:18 -0300

    proyecto inicializado

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import './LandingPage.css'
+
+export const LandingPage = () => {
+    return (
+        <div className='container-landing'>
+            <p className='font-extrabold'>
+                HOLA </p></div>
+    )
+}
+
